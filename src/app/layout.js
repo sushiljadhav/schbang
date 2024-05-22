d19995a59eb1fce415445b996ff628e8c3124dd9
sushiljadhav <sushilj76@gmail.com>
2024-05-22 18:11:25 +0530

    header & banner added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "Travlog",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={nunitoSans.className}>{children}</body>
 		</html>
 	);
 }
